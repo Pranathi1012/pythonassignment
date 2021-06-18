421871c1f47341a09e20e1a27a154519b8421634
Pranathi1012 <85788425+Pranathi1012@users.noreply.github.com>
2021-06-18 09:41:45 +0530

    Add files via upload

---
+++ pythonlists.py
@@ -0,0 +1,34 @@
+list = ["pranathi","19","sathyabama university"]
+list.append("chennai")
+print(list)
+
+a=list.copy()
+print(a)
+
+list1 = ["pranu","20","female"]
+list1.insert(3,"CSE")
+print(list1)
+
+list1.remove("20")
+print(list1)
+
+
+list.pop(1)
+print(list)
+
+list1.reverse()
+print(list1)
+
+list.extend(list1)
+print(list1)
+
+list.sort()
+print(list)
+
+
+list.count(list)
+print(len(list))
+
+list.clear()
+print(list)
+
